f7180146341ed705377e3b402de09edb7e785fb1
RogersCodes <adindajohnrogers@gmail.com>
2025-02-05 00:38:57 +0300

    fix: Corrected client connection in publisher

--- mqtt_publish2.py
+++ mqtt_publish2.py
@@ -1,9 +1,9 @@
 import paho.mqtt.client as mqtt
-from random import randrange, uniform
+from random import randrange
 import time
 
 mqttBroker = "mqtt.eclipseprojects.io"
-client = mqtt.Client("Temperature_Outside")
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "Temperature_Outside")
 client.connect(mqttBroker)
 
 while True:
